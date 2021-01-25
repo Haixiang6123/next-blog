2d89e232ef8349b481735839f06b6abd5ef17929
Haixiang6123 <haixiang6123@gmail.com>
2021-01-25 23:51:19 +0800

    feat: 同构

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import Link from 'next/link';
+import React, {useCallback} from 'react'
+import Link from 'next/link'
+
+// Node.js 和浏览器都会被打印
+console.log('执行了')
 
 export default function FirstPost() {
-    return (
-        <div>
-            First Post
-            <hr/>
-            回到首页
-            <Link href="/">
-                点击这里
-            </Link>
-        </div>
-    )
+  const clickMe = useCallback(() => {
+    // 只会在浏览器里执行
+    console.log('you click me');
+  }, []);
+
+  return (
+    <div>
+      First Post
+      <button onClick={clickMe}>Click Me</button>
+      <hr/>
+      回到首页
+      <Link href="/">
+        点击这里
+      </Link>
+    </div>
+  )
 }
