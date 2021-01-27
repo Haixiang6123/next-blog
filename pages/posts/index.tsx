19ed590d0acf60fbbcbfd8be013dd04313d677ac
Haixiang6123 <haixiang6123@gmail.com>
2021-01-27 09:39:25 +0800

    feat: 动态数据静态化

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,20 +1,31 @@
-import {NextPage} from 'next';
-import {usePosts} from '../../hooks/usePosts';
+import {GetStaticProps, NextPage} from 'next';
+import {getPosts} from 'lib/posts';
 
-const PostsIndex: NextPage = () => {
-  const { loading, empty, posts } = usePosts();
+type Props = {
+  posts: Post[]
+}
+
+const PostsIndex: NextPage<Props> = (props) => {
+  const {posts} = props;
 
   return (
     <div>
       <h1>文章列表</h1>
-      {loading ? <div>加载中</div> :
-        empty ? <div>没有文章</div> :
-        posts.map(p => (
-          <div key={p.id}>{p.id}</div>
-        ))
-      }
+      {posts.map(p => (
+        <div key={p.id}>{p.id}</div>
+      ))}
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts))
+    }
+  }
 }
 
 export default PostsIndex;
