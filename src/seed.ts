9835aefd3b6578a2a65d8fc15a1c844cac78e258
Haixiang <haixiang6123@gmail.com>
2021-01-30 14:21:51 +0800

    feat: 变更为 timestamp

--- src/seed.ts
+++ src/seed.ts
@@ -2,10 +2,12 @@ import 'reflect-metadata'
 import {createConnection} from 'typeorm'
 import {User} from './entity/User'
 import {Post} from './entity/Post'
+import {Comment} from './entity/Comment'
 
 createConnection().then(async connection => {
   const {manager} = connection;
 
+  // 创建用户
   const u1 = new User();
 
   u1.username = 'jack';
@@ -13,6 +15,7 @@ createConnection().then(async connection => {
 
   await manager.save(u1);
 
+  // 创建文章
   const p1 = new Post();
 
   p1.title = 'Post 1'
@@ -20,6 +23,14 @@ createConnection().then(async connection => {
   p1.author = u1;
 
   await manager.save(p1);
+
+  // 创建评论
+  const c1 = new Comment();
+  c1.user= u1;
+  c1.post = p1;
+  c1.content = 'awesome';
+
+  await manager.save(c1);
 
   console.log(u1.id)
 
