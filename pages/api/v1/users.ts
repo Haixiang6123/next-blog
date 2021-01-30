3b9d38b39a92410c754a8fd88c13bbe7bc414fc6
Haixiang <haixiang6123@gmail.com>
2021-01-30 20:52:52 +0800

    feat: 将数据校验放到 User entity

--- pages/api/v1/users.ts
+++ pages/api/v1/users.ts
@@ -1,5 +1,4 @@
 import {NextApiHandler} from 'next'
-import md5 from 'md5'
 import {User} from 'src/entity/User'
 import getDBConnection from 'lib/getDBConnection'
 
@@ -14,47 +13,20 @@ const Users: NextApiHandler = async (req, res) => {
 
   const {username, password, passwordConfirmation} = req.body;
 
-  const errors: UserError = {
-    username: [], password: [], passwordConfirmation: []
-  };
-
-  const found = await connection.manager.findOne(User, {username});
-  if (found) {
-    errors.username.push('已存在，不能重复注册');
-  }
-
-  if (username.trim() === '') {
-    errors.username.push('不能为空');
-  }
-  if (!/[a-zA-Z0-9]*/.test(username.trim())) {
-    errors.username.push('格式不合法');
-  }
-  if (username.trim().length > 42) {
-    errors.username.push('太长');
-  }
-  if (username.trim().length < 3) {
-    errors.username.push('太短');
-  }
-
-  if (password.trim() === '') {
-    errors.password.push('不能为空');
-  }
-  if (password !== passwordConfirmation) {
-    errors.passwordConfirmation.push('密码不匹配')
-  }
-
-  const hasErr = Object.values(errors).some(errList => errList.length > 0);
-
-  if (hasErr) {
-    res.statusCode = 422
-    res.json(errors);
-    return res.end();
-  }
+  console.log('input', passwordConfirmation)
 
   const user = new User();
-
   user.username = username.trim();
-  user.passwordDigest = md5(password);
+  user.password = password;
+  user.passwordConfirmation = passwordConfirmation;
+
+  await user.validate();
+
+  if (user.hasErr()) {
+    res.statusCode = 422
+    res.json(user.errors);
+    return res.end();
+  }
 
   await connection.manager.save(user);
 
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,14 @@
-import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Unique} from 'typeorm'
+import {BeforeInsert, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Unique} from 'typeorm'
 import {Post} from './Post'
 import {Comment} from './Comment'
+import getDBConnection from '../../lib/getDBConnection'
+import md5 from 'md5'
+
+type UserError = {
+  username: string[];
+  password: string[];
+  passwordConfirmation: string[];
+}
 
 @Entity('users')
 export class User {
@@ -24,4 +32,52 @@ export class User {
 
   @OneToMany(type => Comment, comment => comment.user)
   comments: Comment[]
+
+  password: string;
+  passwordConfirmation: string;
+
+  errors: UserError = {
+    username: [],
+    password: [],
+    passwordConfirmation: [],
+  };
+
+  @BeforeInsert()
+  generatePasswordDigest() {
+    this.passwordDigest = md5(this.password);
+  }
+
+  async validate() {
+    // const connection = await getDBConnection();
+    //
+    // const found = await connection.manager.find(User, {username: this.username});
+    //
+    // if (found) {
+    //   this.errors.username.push('已存在，不能重复注册');
+    // }
+
+    if (this.username.trim() === '') {
+      this.errors.username.push('不能为空');
+    }
+    if (!/[a-zA-Z0-9]*/.test(this.username.trim())) {
+      this.errors.username.push('格式不合法');
+    }
+    if (this.username.trim().length > 42) {
+      this.errors.username.push('太长');
+    }
+    if (this.username.trim().length < 3) {
+      this.errors.username.push('太短');
+    }
+
+    if (this.password.trim() === '') {
+      this.errors.password.push('不能为空');
+    }
+    if (this.password !== this.passwordConfirmation) {
+      this.errors.passwordConfirmation.push('密码不匹配')
+    }
+  }
+
+  hasErr() {
+    return Object.values(this.errors).some(errList => errList.length > 0);
+  }
 }
