50d03218d127e37d9005cda207d1b6803984852f
Haixiang <haixiang6123@gmail.com>
2021-01-30 14:16:06 +0800

    feat: 创建 post

--- src/seed.ts
+++ src/seed.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import {createConnection} from 'typeorm'
 import {User} from './entity/User'
+import {Post} from './entity/Post'
 
 createConnection().then(async connection => {
   const {manager} = connection;
@@ -11,6 +12,14 @@ createConnection().then(async connection => {
   u1.passwordDigest = 'xxx';
 
   await manager.save(u1);
+
+  const p1 = new Post();
+
+  p1.title = 'Post 1'
+  p1.content = '我的第一篇文章'
+  p1.author = u1;
+
+  await manager.save(p1);
 
   console.log(u1.id)
 
